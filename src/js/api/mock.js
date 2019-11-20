9ec041e2f3f06d79b152b17dde5ec3e688020b86
Andrew Pelykh <andrew.pelykh@gmail.com>
2019-11-20 22:15:11 +0200

    Update mock.js

--- src/js/api/mock.js
+++ src/js/api/mock.js
@@ -1,14 +1,18 @@
 import data from './data.json';
 
-const withActions = (item) => ({
-  ...item,
-  actions: item.actionIds
-    .map((i) => data.actions.find((action) => action.id === i)),
-});
+const withActions = (id) => {
+  const item = data.resources.find((resource) => resource.id === id);
 
-const get = async (url, params={}) => (
+  return {
+    ...item,
+    actions: item.actionIds
+      .map((i) => data.actions.find((action) => action.id === i)),
+  };
+};
+
+const get = async (url, params = {}) => (
   params.id
-    ? withActions(data.resources[params.id])
+    ? withActions(`${params.id}`)
     : data.resources
 );
 
